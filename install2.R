ea9172fe751ac0aad7d07eaf756dd80695eab1df
Elias <56934916+eliaswestonfarber@users.noreply.github.com>
2026-07-23 16:01:02 -0400

    Update install2.R

--- install2.R
+++ install2.R
@@ -10,6 +10,12 @@ install_if_not_present <- function(pkg) {
   if(!requireNamespace(pkg)){
     if(pkg=="cmdstanr"){
       install.packages(pkg, method='auto', repos='https://mc-stan.org/r-packages/', type="source")
+      library(cmdstanr)
+      install_cmdstan(
+          dir = getwd(), 
+          version = "2.35.0", 
+          cores = 2, overwrite = TRUE
+      )
     } else{
       install.packages(pkg, method='auto', repos='http://cran.us.r-project.org', type="source")
   }
